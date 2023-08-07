10c99d0354f4cebbb46e46e453d1640d2152bc17
airbass <artur.basinski@gmail.com>
2023-08-07 23:56:20 +0200

    adding exeptions and errors

---
+++ exeptions_and_errors/bin/exeptions_and_errors.dart
@@ -0,0 +1,5 @@
+import 'package:exeptions_and_errors/exeptions_and_errors.dart' as exeptions_and_errors;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exeptions_and_errors.calculate()}!');
+}
---
+++ exeptions_and_errors/test/exeptions_and_errors_test.dart
@@ -0,0 +1,8 @@
+import 'package:exeptions_and_errors/exeptions_and_errors.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
